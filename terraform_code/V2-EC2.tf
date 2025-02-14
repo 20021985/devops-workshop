07f50680937578c1d9007da69817248cd1e2564d
rahulkharade <rahul.ibmaix@gmail.com>
2025-02-14 13:46:36 +0000

    new V2-EC2.tf

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-0e82046e2f06c0a68"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "ireland"
     security_groups = [ "demo-sg" ]
 }
 
@@ -26,8 +26,7 @@ resource "aws_security_group" "demo-sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    }
 
   tags = {
     Name = "ssh-prot"
